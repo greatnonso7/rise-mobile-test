b17dd9edc365a0eb4e9fddd463e1bbf7001e03c1
greatnonso7 <greatchinonso7@gmail.com>
2023-05-09 23:06:56 +0100

    Initial commit set up

---
+++ src/types/entities.ts
@@ -0,0 +1,9 @@
+import {NavigatorScreenParams} from '@react-navigation/native';
+
+export type RootStackParamList = {
+  Auth: NavigatorScreenParams<AuthStackParamList>;
+};
+
+export type AuthStackParamList = {
+  Login: undefined;
+};
